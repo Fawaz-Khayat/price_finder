87f92eb96a184e2d1e08e518f297a9f8abc767a5
Fawwaz Khayyat <fawwaz.khayyat@uoit.net>
2019-11-18 22:34:47 -0500

    Product Class

---
+++ app/src/main/java/com/fawwazkhayyat/pricefinder/Product.java
@@ -0,0 +1,58 @@
+package com.fawwazkhayyat.pricefinder;
+
+public class Product {
+    private int barcode;
+    private String name;
+    private double price;
+    private String picture;
+    private String description;
+    private int quantity;
+
+    Product(int barcode){
+        this.barcode = barcode;
+    }
+
+    public int getBarcode() {
+        return barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
