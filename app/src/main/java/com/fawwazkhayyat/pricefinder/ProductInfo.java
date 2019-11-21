427fa0aa331681aec927652244577c224ff60ec7
Fawwaz Khayyat <fawwaz.khayyat@uoit.net>
2019-11-21 12:05:04 -0500

    adding current product to temporary table

--- app/src/main/java/com/fawwazkhayyat/pricefinder/ProductInfo.java
+++ app/src/main/java/com/fawwazkhayyat/pricefinder/ProductInfo.java
@@ -3,6 +3,8 @@ package com.fawwazkhayyat.pricefinder;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,9 +18,13 @@ public class ProductInfo extends AppCompatActivity {
 
     TextView textView_name, textView_description, textView_price, textView_quantity;
     ImageView imageView_product;
-    ImageButton imageButton_decrease, imageButton_increase;
+    //ImageButton imageButton_decrease, imageButton_increase, imageButton_addToBasket;
 
+    int listId;
+    double price, tax, total;
     int quantity;
+
+    String date, time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +51,40 @@ public class ProductInfo extends AppCompatActivity {
         if(quantity>0)
             quantity--;
         textView_quantity.setText(String.valueOf(quantity));
+        total = quantity * price;
     }
     public void imageButton_increase_click(View view){
         quantity++;
         textView_quantity.setText(String.valueOf(quantity));
+        total = quantity * price;
+    }
+    public void imageButton_addToBasket_click(View view){
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        /*
+        INSERT INTO TABLE_NAME (column1, column2, column3,...columnN)
+        VALUES (value1, value2, value3,...valueN);
+         */
+
+        // save to temporary list
+        ContentValues insertData = new ContentValues(6);
+        insertData.put(SQLiteContract.TemporaryList.COLUMN_NAME_DATE, date);
+        insertData.put(SQLiteContract.TemporaryList.COLUMN_NAME_TIME, time);
+        insertData.put(SQLiteContract.TemporaryList.COLUMN_NAME_STORE_ID, storeId);
+        insertData.put(SQLiteContract.TemporaryList.COLUMN_NAME_BARCODE, barcode);
+        insertData.put(SQLiteContract.TemporaryList.COLUMN_NAME_PRICE, price);
+        insertData.put(SQLiteContract.TemporaryList.COLUMN_NAME_QUANTITY, quantity);
+
+        db.insert(SQLiteContract.TemporaryList.TABLE_NAME ,
+                null,
+                insertData);
+        db.close();
+
+        // todo
+        // temporary disable add to basket button
+        // create intent to for basket activity
+        // enable add to basketn button
+        // start basket activity
     }
 }
