077ebbabfdb5dfc954708e01622bc3a191281dc8
Fawwaz Khayyat <fawwaz.khayyat@uoit.net>
2019-11-21 10:24:38 -0500

    add or increase quantity

--- app/src/main/java/com/fawwazkhayyat/pricefinder/ProductInfo.java
+++ app/src/main/java/com/fawwazkhayyat/pricefinder/ProductInfo.java
@@ -5,6 +5,8 @@ import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,8 +14,11 @@ public class ProductInfo extends AppCompatActivity {
     final String barcode = "043100633501";
     final String storeId = "sirc";
 
-    TextView textView_name, textView_description, textView_price;
+    TextView textView_name, textView_description, textView_price, textView_quantity;
     ImageView imageView_product;
+    ImageButton imageButton_decrease, imageButton_increase;
+
+    int quantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,9 @@ public class ProductInfo extends AppCompatActivity {
         textView_name = findViewById(R.id.textView_name);
         textView_description = findViewById(R.id.textView_description);
         textView_price = findViewById(R.id.textView_price);
+        textView_quantity = findViewById(R.id.textView_quantity);
+
+        quantity = 0;
 
         FireStoreViewModel fireStoreViewModel = ViewModelProviders.of(this).get(FireStoreViewModel.class);
         fireStoreViewModel.getProduct(storeId, barcode).observe(this, product -> {
@@ -31,6 +39,15 @@ public class ProductInfo extends AppCompatActivity {
             //textView_description.setText(product.getDescription());
             textView_price.setText("$"+String.valueOf(product.getPrice()));
         });
+    }
 
+    public void imageButton_decrease_click(View view){
+        if(quantity>0)
+            quantity--;
+        textView_quantity.setText(String.valueOf(quantity));
+    }
+    public void imageButton_increase_click(View view){
+        quantity++;
+        textView_quantity.setText(String.valueOf(quantity));
     }
 }
