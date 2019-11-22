b08e47771a9d072bd2c77251b05212f0599506bc
Fawwaz Khayyat <fawwaz.khayyat@uoit.net>
2019-11-22 15:56:39 -0500

    add support for barcode scanner

--- app/src/main/java/com/fawwazkhayyat/pricefinder/Basket.java
+++ app/src/main/java/com/fawwazkhayyat/pricefinder/Basket.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.ArrayList;
 
 public class Basket extends AppCompatActivity {
     RecyclerView recyclerView;
+    TextView textView_result;
 
 
     @Override
@@ -25,6 +33,8 @@ public class Basket extends AppCompatActivity {
                 findViewById(R.id.imageButton_save).setEnabled(false);
             }
         }
+
+        textView_result = findViewById(R.id.textView_result);
 
         DatabaseHelper dbHelper = new DatabaseHelper(this);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
@@ -50,4 +60,25 @@ public class Basket extends AppCompatActivity {
 
     // todo
     // implement button to start scanner activity
+    public void scan_click(View view){
+        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+        intentIntegrator.setOrientationLocked(true);
+        intentIntegrator.initiateScan(); // `this` is the current Activity
+    }
+
+    // Get the scan results:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                textView_result.setText(result.getContents());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
