95f64c63eed0c8fad6a1d09d67602a3ffbd65653
Fawwaz Khayyat <fawwaz.khayyat@uoit.net>
2019-11-25 14:43:16 -0500

    replace code with populateProductInfo()

--- app/src/main/java/com/fawwazkhayyat/pricefinder/ProductInfoEditorActivity.java
+++ app/src/main/java/com/fawwazkhayyat/pricefinder/ProductInfoEditorActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 public class ProductInfoEditorActivity extends ProductInfoActivity {
 
-    String imagePath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +17,6 @@ public class ProductInfoEditorActivity extends ProductInfoActivity {
         quantity = intent.getIntExtra(BasketActivity.EXTRA_QUANTITY,0);
         imagePath = intent.getStringExtra(BasketActivity.EXTRA_IMAGE_PATH);
 
-        textView_name.setText(name);
-        textView_description.setText(description);
-        textView_price.setText(String.valueOf(price));
-        textView_quantity.setText(String.valueOf(quantity));
-
-
-
+        populateProductInfo();
     }
 }
