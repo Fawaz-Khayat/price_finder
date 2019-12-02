13d41fde31554183b48de503ad333785203c4ac5
Fawwaz Khayyat <fawwaz.khayyat@uoit.net>
2019-12-02 00:36:30 -0500

    make Product class parcelable

--- app/src/main/java/com/fawwazkhayyat/pricefinder/Product.java
+++ app/src/main/java/com/fawwazkhayyat/pricefinder/Product.java
@@ -1,11 +1,14 @@
 package com.fawwazkhayyat.pricefinder;
-
+//https://developer.android.com/reference/android/os/Parcelable.html
+//https://stackoverflow.com/questions/6201311/how-to-read-write-a-boolean-when-implementing-the-parcelable-interface
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.Log;
 
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-public class Product {
+public class Product implements Parcelable {
     private String barcode;
     private String name;
     private double price;
@@ -18,6 +21,48 @@ public class Product {
     Product(String barcode){
         this.barcode = barcode;
     }
+
+    private Product(Parcel in){
+        barcode = in.readString();
+        name = in.readString();
+        price = in.readDouble();
+        //check reference at the top of the page
+        isTaxable = in.readByte() != 0;
+        tax = in.readDouble();
+        imageRefPath = in.readString();
+        description = in.readString();
+        quantity = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(barcode);
+        dest.writeString(name);
+        dest.writeDouble(price);
+        //check reference at the top of the page
+        dest.writeByte((byte) (isTaxable ? 1 : 0));
+        dest.writeDouble(tax);
+        dest.writeString(imageRefPath);
+        dest.writeString(description);
+        dest.writeInt(quantity);
+    }
+
+    public static final Parcelable.Creator<Product> CREATOR
+            = new Parcelable.Creator<Product>() {
+        public Product createFromParcel(Parcel in) {
+            return new Product(in);
+        }
+
+        public Product[] newArray(int size) {
+            return new Product[size];
+        }
+    };
+
 
     public String getBarcode() {
         return barcode;
